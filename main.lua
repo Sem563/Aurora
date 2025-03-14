0500b1ec42e1bfe5e9c1d0dd6b134bcc84d878b1
Eiji Ensing <=>
2025-03-14 21:45:17 +0100

    libraries

---
+++ main.lua
@@ -0,0 +1,7 @@
+local Vector = require("libraries.brinevector.brinevector")
+local Camera = require("libraries.hump.camera")
+local ECS = require("libraries.ECS.ECS")
+
+local World, System, Query, Component = ECS.World, ECS.System, ECS.Query, ECS.Component
+
+System
